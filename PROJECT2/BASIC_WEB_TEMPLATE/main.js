d00f805e5ed0ad582e6a8b154a1714083e97d36f
Akua Konadu <130577072+ADO-Tech-Web@users.noreply.github.com>
2023-10-02 18:15:09 +0100

    Added the nav pages to navigate through the site

---
+++ PROJECT2/BASIC_WEB_TEMPLATE/main.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    $("#pro1").hover(function(){   // hover
+    $("#pro1").css("background-color","yellow");
+    }
+    , // used to separate the two function
+    function()
+    {
+    $("#pro1").css("background-color","lightgrey"); // not hover
+    });
+    
+    
+    $("#prolast").hover(function(){   // hover
+    $("#prolast").css("background-color","yellow");
+    }
+    , // used to separate the two function
+    function()
+    {
+    $("#prolast").css("background-color","white"); // not hover
+    });
+    
+    $("#prometa1").hover(function(){   // hover
+    $("#prometa1").css("background-color","lightgreen");
+    }
+    , // used to separate the two function
+    function()
+    {
+    $("#prometa1").css("background-color","white"); // not hover
+    });
+    
+    
+$("#prometa2").hover(function(){   // hover
+    $("#prometa2").css("background-color","lightblue");
+    }
+    , // used to separate the two function
+    function()
+    {
+    $("#prometa2").css("background-color","white"); // not hover
+    });
+    
+    
+    
+    
+    
+    $("#prometa3").hover(function(){   // hover
+    $("#prometa3").css("background-color","orange");
+    }
+    , // used to separate the two function
+    function()
+    {
+    $("#prometa3").css("background-color","white"); // not hover
+    });
+    
+    
+    
+    });
